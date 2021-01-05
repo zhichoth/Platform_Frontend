2ca2594d187f806f4df81196aadc07846656e929
Ysec dev <dev5@ysec.finance>
2021-01-05 22:23:57 +0100

    yield dashboard

--- src/components/views/yield/YieldPool.Yield.vue
+++ src/components/views/yield/YieldPool.Yield.vue
@@ -1,14 +1,90 @@
 :<template>
   <div id="yield">
     <div class="block px-4 mt-6 sm:px-6 lg:px-8">
-      <div class="dark:bg-gray-800 p-3 py-10 rounded-lg">
+      <div class="dark:bg-gray-800 bg-gray-50 p-3 py-10 rounded-lg  border border-gray-200">
+        <Chart style="height: 200px; width: 100%;" :chartData="chartData" :options="options" />
+      </div>
+    </div>
+    <div class="block px-4 mt-3 sm:px-6 lg:px-8">
+      <div class="dark:bg-gray-800 bg-gray-50 p-3 py-10 rounded-lg border border-gray-200">
         <Chart style="height: 200px; width: 100%;" :chartData="chartData" :options="options" />
       </div>
     </div>
     <div class="block px-4 mt-6 sm:px-6 lg:px-8">
-      <div class="dark:bg-gray-800 p-3 py-10 rounded-lg">
-        <Chart style="height: 200px; width: 100%;" :chartData="chartData" :options="options" />
-      </div>
+      <ul class="grid grid-cols-1 gap-4 sm:gap-6 sm:grid-cols-2 xl:grid-cols-3 mt-3">
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">655,923.55632</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">Total $YSEC staked</span>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">11,923.55632</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">Your $YSEC staked</span>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">6,3946</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">Est. rewards (in ETH)</span>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">373</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">ETH in pool</span>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">27</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">Presale(s) launched</span>
+            </h1>
+          </div>
+        </li>
+        <li>
+          <div class="max-w-5xl mx-auto px-4 sm:px-4 lg:px-4 rounded-tl-lg rounded-tr-lg bg-gray-50 dark:bg-gray-800 text-center border border-gray-200 border-b-0">
+              <h1 class="inline-block p-10 text-4xl">
+                <span class="block text-gray-800 dark:text-white font-semibold tracking-wide uppercase">123</span>
+              </h1>
+          </div>
+          <div class="flex-root pt-2 pb-2 pl-6 rounded-bl-lg rounded-br-lg bg-yellow-500 text-center">
+            <h1 class="inline-block">
+              <span class="block text-base text-white font-semibold tracking-wide uppercase">Participant(s)</span>
+            </h1>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
